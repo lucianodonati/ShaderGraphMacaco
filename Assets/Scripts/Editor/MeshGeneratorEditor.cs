c9f9a67c68f3b1515d40083308d8d8901e0b0fd5
Luciano Donati <me@lucianodonati.com>
2020-07-02 14:15:22 -0300

    Agregados scripts de generacion de mesh y editor (boton)

---
+++ Assets/Scripts/Editor/MeshGeneratorEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(MeshGenerator))]
+    public class MeshGeneratorEditor : UnityEditor.Editor
+    {
+        private MeshGenerator generator = null;
+
+        private void Awake()
+        {
+            if (null == generator)
+                generator = target as MeshGenerator;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Create Mesh"))
+                generator.CreateMesh();
+        }
+    }
+}
---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class MeshGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private MeshFilter filter;
+
+    [SerializeField]
+    private Vector3[] vertices =
+    {
+        new Vector3(-.5f, 0, -.5f),
+        new Vector3(-.5f, 0, .5f),
+        new Vector3(.5f, 0, -.5f),
+        new Vector3(.5f, 0, .5f)
+    };
+
+    [SerializeField]
+    Color[] colors = {Color.black, Color.blue, Color.red, Color.yellow};
+
+    [SerializeField]
+    private int[] triangles = {0, 1, 2, 1, 3, 2};
+
+    [SerializeField]
+    private Vector2[] uv =
+    {
+        new Vector2(-.5f, -.5f),
+        new Vector2(-.5f, .5f),
+        new Vector2(.5f, -.5f),
+        new Vector2(.5f, .5f)
+    };
+
+    [SerializeField]
+    private float testVerticalVerticeCero = 0;
+
+    private Mesh mesh;
+
+
+    private void OnValidate()
+    {
+        if (null == filter)
+        {
+            filter = GetComponent<MeshFilter>();
+        }
+
+        vertices[0] = new Vector3(vertices[0].x, testVerticalVerticeCero, vertices[0].z);
+
+        CreateMesh();
+    }
+
+
+    public void CreateMesh()
+    {
+        if (null != mesh)
+            mesh.Clear();
+        else
+        {
+            mesh = new Mesh();
+            filter.mesh = mesh;
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colors;
+        mesh.uv = uv;
+
+        mesh.RecalculateNormals();
+    }
+}
